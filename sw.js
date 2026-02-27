30ab61f218f5f3abc11620220620318fb0e881d4
Taqi Hamoda <46078134+TaqiHamoda@users.noreply.github.com>
2026-02-27 16:20:29 +0100

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'imposter-v1';
+const ASSETS = [
+  'index.html',
+  'data.json',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
